d416d10d7b4280ada3d0963066a3ffe1f53a5854
LuoChen <luochen1990@gmail.com>
2024-07-14 13:15:15 +0800

    improve: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,78 @@
 {
-  description = "A Python development environment with pip";
+  description = "A Python development environment with pip & poetry";
 
   inputs = {
     nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        python3 = pkgs.python3.withPackages (p: with p; [
-          # Add your required Python packages here
-          requests
-          numpy
-          # Add more packages as needed
-        ]);
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            python3
-            pkgs.python3Packages.pip
-            pkgs.python3Packages.virtualenv
-            # Add other tools you need for development
-            pkgs.git
-          ];
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
+      inherit system;
+      pkgs = import nixpkgs { inherit system; };
+      hpkgs = pkgs.haskell.packages.ghc928;
+      python3 = pkgs.python3.withPackages (p: with p; [
+        # Add your required Python packages here
+        requests
+        numpy
+        # Add more packages as needed
+      ]);
+      pyver = pkgs.python3.version;
+    });
+  in
+  {
+    devShells = eachSystem ({pkgs, python3, pyver, ...}: rec {
+      default = poetry;
 
-          shellHook = ''
-            echo "Welcome to your Python development environment with pip!"
-          '';
-        };
-      });
+      poetry = pkgs.mkShell {
+        buildInputs = [
+          python3
+          pkgs.poetry
+          # Add other tools you need for development
+          pkgs.git
+        ];
+
+        shellHook = ''
+          echo "Welcome to your Python development environment with poetry!"
+
+          #export PROMPT_COMMAND='PS1="\[\033[01;32m\]venv-py${pyver}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ ";unset PROMPT_COMMAND'
+          export PROMPT_COMMAND='PS1="";unset PROMPT_COMMAND'
+
+          export PATH=$(poetry env info --path):$PATH
+        '';
+      };
+
+      pip = pkgs.mkShell {
+        buildInputs = [
+          python3
+          pkgs.python3Packages.pip
+          pkgs.python3Packages.virtualenv
+          pkgs.python3Packages.setuptools
+          pkgs.python3Packages.wheel
+          # Add other tools you need for development
+          pkgs.git
+        ];
+
+        shellHook = ''
+          echo "Welcome to your Python development environment with pip!"
+
+          export PROMPT_COMMAND='PS1="\[\033[01;32m\]venv-py${pyver}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ ";unset PROMPT_COMMAND'
+
+          python3 -m venv --system-site-packages ${builtins.toString ./.venv-py${pyver}}
+          source ${builtins.toString ./.venv-py${pyver}/bin/activate}
+
+          export PIP_EXTRA_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
+
+          if [ -f setup.py ] || [ -f setup.cfg ]; then
+            echo "Detected setuptools project. Initializing pip environment..."
+            pip install -e .
+          elif [ -f requirements.txt ]; then
+            echo "Detected pip project. Initializing pip environment..."
+            pip install -r requirements.txt
+          fi
+        '';
+      };
+    });
+  };
 }
