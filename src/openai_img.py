6b87b7552f610d6cd96ef4cd7df24c35d39b0675
LuoChen <luochen1990@gmail.com>
2024-07-14 15:02:22 +0800

    add: hello.py & openai_img.py

--- src/openai_img.py
+++ src/openai_img.py
@@ -1,10 +1,10 @@
 from funix import funix                      # add line one
 from IPython.display import Image
 from openai import OpenAI                    # pip install openai
-
 import os
-client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
 
+# 初始化OpenAI客户端
+client = OpenAI()
 
 @funix()                                     # add line two
 def dalle(prompt: str = "a cat") -> Image:
